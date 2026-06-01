1b068ba66340d65ed1eb026093165c0d2ac812f3
azurecat6 <6azurecat6@gmail.com>
2026-06-01 22:41:21 +0300

    добавила методы, а также добавила зависимость для JpaRepository

--- src/main/java/domain/user/service/UserService.java
+++ src/main/java/domain/user/service/UserService.java
@@ -1,4 +1,19 @@
 package domain.user.service;
 
+import com.sun.tools.javac.util.List;
+import domain.user.entity.User;
+
+import java.util.Optional;
+
 public interface UserService {
+
+    Optional<User> getUserById(long id);
+    List<User> getAllUsers();
+    User updateUser(User user);
+    User createUser(User user);
+    void deleteUser(long id);
+    User changePassword(User user);
+
+
+
 }
