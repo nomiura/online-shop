eb67484a70113943889ec1129146cd361c4ad146
azurecat6 <6azurecat6@gmail.com>
2026-06-01 22:28:12 +0300

    добавила методы, а также добавила зависимость для JpaRepository

--- src/main/java/domain/user/repository/UserRepository.java
+++ src/main/java/domain/user/repository/UserRepository.java
@@ -1,13 +1,16 @@
 package domain.user.repository;
 
+import domain.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-import ru.onlineshop.domain.user.entity.User;
+import java.util.Optional;
 
-public interface UserRepository {
+@Repository
+public interface UserRepository extends JpaRepository {
 
-    User persist(User user);
-    User findById(long id);
-    User findByPhone(int phone);
-    User findByEmail(String email);
-    User findAll();
+    Optional<User> persist(User user);
+    Optional<User> findByUsername(String username);
+    Optional<User> findByPhone(int phone);
+    Optional<User> findByEmail(String email);
 }
