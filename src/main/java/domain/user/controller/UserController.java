e6112edfe3ece1f7fd5fb8942dd0da73af927ff2
azurecat6 <6azurecat6@gmail.com>
2026-06-05 17:47:16 +0300

    merge dev into feature/azure

--- src/main/java/domain/user/controller/UserController.java
+++ src/main/java/domain/user/controller/UserController.java
@@ -5,6 +5,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import org.springframework.web.bind.annotation.RestController;
+
 @RestController
 public class UserController {
 
--- src/main/java/domain/user/repository/UserRepository.java
+++ src/main/java/domain/user/repository/UserRepository.java
@@ -8,8 +8,4 @@ import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-
-    Optional<User> persist(User user);
-    Optional<User> findByUsername(String username);
-    Optional<User> findByPhone(int phone);
 }
