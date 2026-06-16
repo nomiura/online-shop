d14d9a74eb922d37a59c9a72de11654deb974ca5
nomiura <niruritsu@gmail.com>
2026-06-16 18:40:55 +0700

    Создание пакетов для тестов, начала писать тесты на AccountController

--- src/main/java/domain/dto/request/AccountCreateRequest.java
+++ src/main/java/domain/dto/request/AccountCreateRequest.java
@@ -4,6 +4,7 @@ package domain.dto.request;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
