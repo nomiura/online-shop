43109c79d8bfcaecd351e764ea0ed5dcda409f87
azurecat6 <6azurecat6@gmail.com>
2026-07-14 22:19:38 +0300

    Продолжаю работу над Product, заменила название поля Id, а также сделал ренейм по всему проекту, конфликтов нет

--- src/main/java/domain/mapper/ProductMapper.java
+++ src/main/java/domain/mapper/ProductMapper.java
@@ -1,4 +1,37 @@
 package domain.mapper;
 
+import domain.dto.request.CreateProductRequest;
+import domain.dto.response.ProductResponse;
+import domain.entity.Product;
+import org.springframework.stereotype.Component;
+
+
+@Component
 public class ProductMapper {
+
+    public ProductResponse toResponse(Product product) {
+        if(product == null) return null;
+        return new ProductResponse(product.getProductId(),
+                product.getName(),
+                product.getDescription(),
+                product.getCurrentPrice(),
+                product.getDiscountPercent(),
+                product.getEffectivePrice(),
+                product.getImage(),
+                product.isInStock()
+        );
+    }
+
+    public Product toEntity(CreateProductRequest request) {
+        if (request == null) return null;
+          Product product = new Product();
+          product.setName(request.getName());
+          product.setDescription(request.getDescription());
+          product.setCurrentPrice(request.getCurrentPrice());
+          product.setDiscountPercent(request.getDiscountPercent());
+          product.setImage(request.getImage());
+          product.isInStock();
+          product.setQuantityAvailable(request.getQuantityAvailable());
+        return  product;
+    }
 }
